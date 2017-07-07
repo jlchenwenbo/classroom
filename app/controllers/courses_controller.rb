09004a0f1b3c4d913f8a643ffa278dd60082aa75
Wenbo <jlchenwenbo@gmail.com>
2017-07-07 09:30:55 -0700

    update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -4,4 +4,28 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  def new
+    ＠course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def course_params
+      params.require(:course).permit(:title, :description)
+    end
+
 end
--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe CoursesController, type: :controller do
+RSpec.describe CoursesController do
 
   describe "GET index" do
+
     it "assigns @courses and render template" do
       course1=create(:course)
       course2=create(:course)
@@ -10,9 +11,83 @@ RSpec.describe CoursesController, type: :controller do
       get :index
 
       expect(assigns[:courses]).to eq([course1, course2])
+    end
+
+    it "render template" do
+      course1=create(:course)
+      course2=create(:course)
+
+      get :index
+
       expect(response).to render_template("index")
+    end
+  end
+
+  describe "GET show" do
+    it "assigns @course" do
+      course = create(:course)
+
+      get :show, params: {id: course.id }
+
+      expect(assigns[:course]).to eq(course)
+    end
+
+    it "render template" do
+      course = create(:course)
+
+      get :show, params: { id: course.id}
+
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET new" do
+
+    it "assign @course" do
+      course = build(:course)
+
+      get :new
+
+      expect(assigns(:course)).to be_a_new(Course)
+    end
+
+    it "render template" do
+      course = build(:course)
+
+      get :new
+
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST create" do
+
+    context "when course doesn't have a title" do
+
+      it "doesn't create a record" do
+        expect { post :create, course: {:description => "bar"}}.to change{Course.count}.by(0)
+      end
+
+      it "render new template" do
+        post :create, course: {:description => "bar"}
+        expect(response).to render_template("new")
+      end
+    end
+
+    context "when course have a title" do
+      it "create a new course record" do
+        course = FactoryGirl.build(:course)
+        expect { post :create, course: FactoryGirl.attributes_for(:course) }.to change{ Course.count }.by(1)
+      end
+
+      it "redirect to courses_path" do
+        course = FactoryGirl.build(:course)
+        post :create, course: FactoryGirl.attributes_for(:course)
+        expect(response).to redirect_to courses_path
+      end
 
     end
+
   end
 
 end
